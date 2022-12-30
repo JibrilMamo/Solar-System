7afd319ecdede9d459fcbac3ed3a90b545ce7fc3
JibrilMamo <107517102+JibrilMamo@users.noreply.github.com>
2022-12-30 01:33:22 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+
+# Imports
 import pygame
 import math
+
+# Initializes pygame
 pygame.init()
 
+# Colors
 BLACK = (0, 0, 0)
 GRAY = (127, 127, 127)
 WHITE = (255, 255, 255)
@@ -12,33 +17,41 @@ YELLOW = (255, 255, 0)
 CYAN = (0, 255, 255)
 MAGENTA = (255, 0, 255)
 TAN = (200,150,100)
+
+# Dimensions of window
 width, height = 1000, 1000
+
+# Creates window
 window = pygame.display.set_mode((width, height))
 
+# List to store planets
 PLANETS = []
 
+# Sun
 sun_mass = (1.98847*10**30)
-earth_mass = 5.9736*10**24 #sun_mass*.00000300411874456241
-mars_mass = 6.4185*10**23 #sun_mass*3.2278586049e-7
-venus_mass = 4.8685*10**24 #sun_mass*1.6606737844e-7
-mercury_mass = 3.3022*10**23 #sun_mass*0.00000244836482321
-sun_radius = 30
+sun_radius = 40
 sun_posx,sun_posy = width//2,height//2
 
-
-mur_mass = 3.285 * 10**23
-mur_radius = 5
-
+# Fonts
 FONT = pygame.font.SysFont("comicsans", 16)
 FONT2 = pygame.font.SysFont("comicsans", 30)
 
+# Planet class
 class Planets:
+    # Distance of an AU in meters
     AU = 149.6e6 *1000
+
+    # Gravitational constant
     G = 6.67428e-11
+
+    # Scale of the solar system
     Scale = 250/AU #100
+
+    # Time step of simulation
     TimeStep = 3600*24
 
-    def __init__(self, x, y, radius,mass,color):#x_vel,y_vel):
+    # Initializer
+    def __init__(self, x, y, radius,mass,color, image):
         self.x  = x
         self.y  = y
         self.color = color
@@ -49,14 +62,25 @@ class Planets:
         self.orbit =[]
         self.sun = False
         self.distance_to_sun = 0
+        self.image = image
 
-    def draw(self, win, draw_dist = False,draw_line = False):
+    # Draws the planet
+    def draw(self, win, draw_dist = False,draw_line = False, Display_image=True):
         x = self.x *self.Scale +width/2
         y = self.y * self.Scale+height/2
 
+        # Displays the image of the planet if set to True
+        if Display_image:
+            image1 = pygame.image.load(self.image)
+            image = pygame.transform.scale(image1, (int(self.radius*2), int(self.radius*2)))
+            image.set_colorkey(BLACK)
+            win.blit(image, (x-self.radius, y-self.radius))
 
-        pygame.draw.circle(win, self.color, (x, y), self.radius)
+        # Draws the circle of the planet if set to True
+        if not Display_image:
+            pygame.draw.circle(win, self.color, (x, y), self.radius)
 
+        # Draws the orbit of the planet if set to True
         if draw_line:
             if len(self.orbit) > 2:
                 updated_points = []
@@ -66,14 +90,17 @@ class Planets:
                     y = y * self.Scale + height / 2
                     updated_points.append((x, y))
                 pygame.draw.lines(win, self.color, False, updated_points, 2)
+
+        # Draws the distance to the sun if set to True
         if draw_dist:
             if not self.sun:
                 distance_text = FONT.render(f"{round(self.distance_to_sun / 1000, 1)}km", 1, WHITE)
                 win.blit(distance_text, (x - distance_text.get_width() / 2, y - distance_text.get_height() / 2))
 
+    # Moves the planet
     def move(self):
         self.x += self.x_vel
-        self.y +=0# self.y_vel
+        self.y +=0
 
     def attraction(self,other):
         other_x,other_y = other.x,other.y
@@ -131,10 +158,11 @@ def set_planet_position(x,y,planet):
 
 def draw(win,count):
     win.fill((0,0,0))
+    count_d = count - (count//365)*365
     count_y = count//365
 
 
-    elapsed_time = FONT2.render(f'Days: {count} Years: {count_y}', 1, WHITE)
+    elapsed_time = FONT2.render(f'Days: {count_d} Years: {count_y}', 1, WHITE)
     win.blit(elapsed_time, (width -70 - elapsed_time.get_width(), 40))
 
 
@@ -145,24 +173,25 @@ def main():
     run=True
     count = 0
     clock = pygame.time.Clock()
-    sun = Planets(0,0,sun_radius,sun_mass,YELLOW)
+    sun = Planets(0,0,sun_radius,sun_mass,YELLOW,'sun_img.png')
     sun.sun = True
-    earth = Planets(-1*Planets.AU, 0  , sun_radius*16/30, earth_mass, BLUE)
+    earth = Planets(-1*Planets.AU, 0  , (sun_radius*16/30), (sun_mass*.00000300411874456241), BLUE,'earth_img.png')
     earth.y_vel = 29.783 * 1000
 
-    mars = Planets(-1.524*Planets.AU, 0, sun_radius* 12/30, mars_mass, RED)
+    mars = Planets(-1.524*Planets.AU, 0, (sun_radius* 12/30), (sun_mass*3.2278586049e-7), RED,'mars_img.png')
     mars.y_vel = 24.077 * 1000
 
-    venus = Planets(.723*Planets.AU, 0, sun_radius* 14/30, venus_mass, GRAY)
+    venus = Planets(.723*Planets.AU, 0, (sun_radius* 14/30), (sun_mass*1.6606737844e-7), GRAY, 'venus_img.png')
     venus.y_vel = -35.02 * 1000
 
-    mercury = Planets(.387*Planets.AU, 0, sun_radius*8/30, mercury_mass, TAN)
+    mercury = Planets(.387*Planets.AU, 0, (sun_radius*8/30), (sun_mass*0.00000244836482321), TAN, 'mercury_img.png')
     mercury.y_vel = -47.4 * 1000
 
     planets = [sun,earth,mars,venus,mercury]
 
     while run:
         clock.tick(60)
+
         count +=int(Planets.TimeStep/(3600*24))
         draw(window,count)
 
